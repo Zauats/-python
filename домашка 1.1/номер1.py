814bf5f94636d5827faab06d472f48b910cf4768
Zauats <33497760+Zauats@users.noreply.github.com>
2019-01-31 21:56:24 +0300

    Update номер1.py

--- домашка 1.1/номер1.py
+++ домашка 1.1/номер1.py
@@ -1,39 +1,25 @@
-from math import inf
 
 
-# номер 1
-# По-моему я здесь что-то намудрил, можно сделать все гораздо проще.
-def adv_print (*args, max_line=inf, in_file=False, start='', sep=' ', end='\n',):
-    print(start, end='')
-    for obj in args:
-        obj = str(obj)
-        if max_line != inf:
-                # print(obj, end='')
-            1+1
-        else:
+def adv_print(*args, max_line=False, in_file=False, start='', sep=" ", end="\n", file=sys.stdout, flush=False):
+    all_str = start
+    for item in args:
+        all_str += str(item) + sep
+    print(all_str)
 
-            str_min_index = 0
-            str_max_index = max_line
-            loop = True
+    if max_line == False:
+        print(all_str, end=end, sep='', file=file, flush=flush)
 
-            while loop:
-                if len(obj) < str_min_index:
-                    loop = False
-                elif len(obj) < str_max_index:
-                    print(obj[str_min_index:])
-                    loop = False
-                else:
-                    # print(obj[str_min_index:str_max_index], end='')
-                    str_max_index +=max_line
-                    str_min_index +=max_line
+    else:
+        start_index = 0
+        while max_line < len(all_str) - start_index:
+            print(all_str[start_index : max_line + start_index], sep='', file=file, flush=flush)
+            start_index += max_line
 
-
-        print(sep, end='')
-
-    print(end, end='')
+        print(all_str[start_index:], end=end, sep='', file=file, flush=flush)
 
     if in_file:
         file = input('введите путь к файлу')
         with open(file, 'w')  as f:
             for obj in args:
                 f.write(obj)
+
