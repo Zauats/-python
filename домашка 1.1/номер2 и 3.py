514aaa11a6a5187957a98f84157b03508ad622a6
Zauats <33497760+Zauats@users.noreply.github.com>
2019-02-01 18:18:48 +0300

    Update номер2 и 3.py

--- домашка 1.1/номер2 и 3.py
+++ домашка 1.1/номер2 и 3.py
@@ -1,57 +1,72 @@
-#задание 2
+
+# задание 1
+
 class Contact():
-    contact_list = []
-
     def __init__(self, name, surname, phone_number, special_contact = False, **kwargs):
-        contact = {
-            name: name,
-            surname: surname,
-            phone_number: phone_number,
-            special_contact: special_contact,
+        self.name = name
+        self.surname = surname
+        self.phone_number = phone_number
+        self.special_contact = special_contact
+        self.additional_information = kwargs
 
-        }
-        if len(kwargs) > 0:
-            for key, value in kwargs.items():
-                self.contact[key] = value
-        self.contact_list.append(contact)
+    def __str__(self):
+        print("Имя:", self.name)
+        print("Фамилия:", self.surname)
+        print("Телефон:", self.phone_number)
 
-    def contact_print(self):
-        for cont in self.contact_list:
-            print('Имя: ', self.cont['name'])
-            print('Фамилия: ', self.cont['surname'])
-            print('Телефон: ', self.cont['phone_number'])
-            print('В избранных: ', self.cont['special_contact'])
-            print('Дополнительная информация: ')
-            print('    telegram: ', self.cont['telegram'])
-            print('    email: ', self.cont['email'])
+        if self.special_contact == False:
+            print("В избранных: нет")
+        else:
+            print("В избранных: да")
 
-    def append(self, name, surname, phone_number, special_contact=False, **kwargs):
-        contact = {
-            name: name,
-            surname: surname,
-            phone_number: phone_number,
-            special_contact: special_contact,
+        print("Дополнительная информация:")
+        for key, value in self.additional_information.items():
+            print("\t\t", key, " : ", value, sep="")
 
-        }
-        if len(kwargs) > 0:
-            for key, value in kwargs.items():
-                self.contact[key] = value
-        self.conts.append(contact)
+        return ''
+
+jhon = Contact('Jhon', 'Smith', '+71234567809', telegram='@jhony', email='jhony@smith.com')
+print(jhon)
+
+
+#задание 2
+
+class PhoneBook():
+
+    def __init__(self, name):
+        self.name = name
+        self.contacts_list = []
+
+    def contacts_print(self):
+        for contact in self.contacts_list:
+            print(contact)
+
+    def append_phone(self, contact):
+        if isinstance(jhon, Contact):
+            self.contacts_list.append(contact)
+        else:
+            print("Ошибка, нужен класс, типа Contact")
 
     def delete(self, number):
         index = 0
-        for cont in self.contact_list:
-            if number == cont['phone_number']:
-                self.contact_list.pop(index)
+        for contact in self.contacts_list:
+            if number == contact.phone_number:
+                self.contacts_list.pop(index)
             index += 1
 
     #задание 3
     def find_special(self):
-        for cont in self.contact_list:
-            if cont['special_contact'] == True:
-                self.contact_print()
+        for cont in self.contacts_list:
+            if cont.special_contact:
+                print(cont)
 
     def find_name(self, name, surname):
-        for cont in self.contact_list:
-            if cont['name'] == name and cont['surname'] == surname:
-                self.contact_print()
+        for cont in self.contacts_list:
+            if cont.name == name and cont.surname == surname:
+                print(cont)
+
+a = PhoneBook("Книжка")
+a.append_phone(jhon)
+a.contacts_print()
+a.delete('+71234567809')
+a.contacts_print()
