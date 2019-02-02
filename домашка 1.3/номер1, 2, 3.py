2ba063cee80f6d391e21327ee0ea4ae2b3f0ed46
Zauats <33497760+Zauats@users.noreply.github.com>
2019-02-02 15:33:18 +0300

    Update номер1, 2, 3.py

--- домашка 1.3/номер1, 2, 3.py
+++ домашка 1.3/номер1, 2, 3.py
@@ -4,7 +4,7 @@ import datetime, sys
 def path_decorator(file_path):
     def decorator(old_function):
         def new_function(*args, **kwargs):
-            data = old_function()
+            data = old_function(*args, **kwargs)
             with open(file_path, 'w', encoding='UTF-8') as f:
                 f.write('время вызова функции: ' + str(datetime.datetime.now()) + '\n')
                 f.write('Имя функции: ' + str(old_function.__name__) + '\n')
@@ -18,9 +18,8 @@ def path_decorator(file_path):
 
 # задание 1
 def decorator(old_function):
-
     def new_function(*args, **kwargs):
-        data = old_function(args, kwargs)
+        data = old_function(*args, **kwargs)
         with open('file.txt', 'w', encoding='UTF-8') as f:
             f.write('время вызова функции: ' + str(datetime.datetime.now()) + '\n')
             f.write('Имя функции: ' + str(old_function.__name__) + '\n')
@@ -31,12 +30,11 @@ def decorator(old_function):
 
 
 # задание 3
-@decorator
+@path_decorator('file.txt')
 def adv_print(*args, max_line=False, in_file=False, start='', sep=" ", end="\n", file=sys.stdout, flush=False):
     all_str = start
     for item in args:
         all_str += str(item) + sep
-    print(all_str)
 
     if max_line == False:
         print(all_str, end=end, sep='', file=file, flush=flush)
@@ -55,5 +53,5 @@ def adv_print(*args, max_line=False, in_file=False, start='', sep=" ", end="\n",
             for obj in args:
                 f.write(obj)
 
-adv_print(123)
+adv_print(123, max_line=2)
 
