3c25ac8cedc6e1d71fdde6bcb1dcf7186e74c934
Zauats <33497760+Zauats@users.noreply.github.com>
2019-02-11 15:48:30 +0300

    Update main.py

--- homework_p1n4/number_1_2/main.py
+++ homework_p1n4/number_1_2/main.py
@@ -1,5 +1,5 @@
-from homework_p1n4.number_1_2.application.salary import calculate_salary
-from homework_p1n4.number_1_2.application.db.people import get_employees
+from application.salary import calculate_salary
+from application.db.people import get_employees
 
 
 
