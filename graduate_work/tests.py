84b4ff0fc00a8c59e4a8b052c07228c9b9bf7bb3
Zauats <33497760+Zauats@users.noreply.github.com>
2019-04-05 17:49:40 +0300

    Update tests.py

--- graduate_work/tests.py
+++ graduate_work/tests.py
@@ -3,51 +3,33 @@ import unittest
 
 class UserTests(unittest.TestCase):
 
-    def setUp(self): # Есть ли метод, который выполняется только один раз в начале? Немного погуглил и не нашел
-        print("Для проведения тестов, требуется авторизация в вконтакте")
-        login = input("Введите логин: ")
-        password = input("Введите пароль: ")
-        id = int(input("Введите id: "))
-
-        self.user = User(login, password, id)
-
-    def test_id(self):
-        self.assertEqual(self.user.user['id'], self.user.id,)
+    def setUp(self):
+        self.user = {
+            'bdate': '28.1.2002',
+            'first_name': 'Александр',
+            'id': 187509567,
+            'last_name': 'Зайцев',
+            'sex': 2, }
 
     def test_user_class(self):
-        self.assertIsInstance(self.user.user, dict)
-
-    def test_age(self):
-        self.assertIn('age', self.user.user)
-        self.assertIsInstance(self.user.user['age'], int)
-        self.assertGreater(self.user.user['age'], 0)
-
-    def test_groups(self):
-        self.assertIn('groups', self.user.user)
-        self.assertIsInstance(self.user.user['groups'], list)
-
-    def test_search_sex(self):
-        self.assertIn('search_sex', self.user.user)
-        self.assertIsInstance(self.user.user['search_sex'], int)
-        self.assertGreater(self.user.user['search_sex'], -1)
-        self.assertLess(self.user.user['search_sex'], 3)
+        self.assertIsInstance(self.user, dict)
 
     def test_get_age(self):
-        self.assertRaises(ValueError, self.user.get_age, 'строка')
-        self.assertRaises(ValueError, self.user.get_age, 12345)
-        self.assertRaises(ValueError, self.user.get_age, [1, 2, 'строка'])
-        self.assertIsInstance(self.user.get_age(self.user.user), int)
+        self.assertRaises(ValueError, User.get_age, 'строка')
+        self.assertRaises(ValueError, User.get_age, 12345)
+        self.assertRaises(ValueError, User.get_age, [1, 2, 'строка'])
+        self.assertGreater(User.get_age(self.user), 0)
 
     def test_get_groups(self):
-        self.assertRaises(ValueError, self.user.get_groups, 'строка')
-        self.assertRaises(ValueError, self.user.get_groups, 12345)
-        self.assertRaises(ValueError, self.user.get_groups, [1, 2, 'строка'])
-        self.assertIsInstance(self.user.get_groups(self.user.user), list)
+        self.assertRaises(ValueError, User.get_groups, 'строка')
+        self.assertRaises(ValueError, User.get_groups, 12345)
+        self.assertRaises(ValueError, User.get_groups, [1, 2, 'строка'])
+        self.assertIsInstance(User.get_groups(self.user), list)
 
     def test_get_seatch_sex(self):
-        self.assertIsInstance(self.user.get_seatch_sex(self.user.user), int)
-        self.assertGreater(self.user.get_seatch_sex(self.user.user), -1)
-        self.assertLess(self.user.get_seatch_sex(self.user.user), 3)
+        self.assertIsInstance(User.get_seatch_sex(self.user), int)
+        self.assertGreater(User.get_seatch_sex(self.user), -1)
+        self.assertLess(User.get_seatch_sex(self.user), 3)
 
 if __name__ == "__main__":
     unittest.main()
